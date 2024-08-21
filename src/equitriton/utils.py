1827344005c6178bc02113df56dd2684fce04adc
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-08-21 13:53:41 -0700

    refactor: modifying num block calculation with matching arithmetic

--- src/equitriton/utils.py
+++ src/equitriton/utils.py
@@ -52,6 +52,14 @@ def calculate_lastdim_num_blocks(input_tensor: torch.Tensor, block_size: int) ->
     Calculate the number of blocks for a tensor, assuming we
     stride along the last dimension, and a given block size.
 
+    The corresponding pointer arithmetic looks like this:
+
+    ```python
+    block_id = tl.program_id(0)
+    striding = tl.arange(0, block_size) * stride
+    offset = (striding + (block_size * stride * block_id))
+    ```
+
     This function is used to work out the amount of parallel
     work that needs to be done, given as the total number of
     elements divided by the last dimension stride, and a specified
@@ -68,10 +76,10 @@ def calculate_lastdim_num_blocks(input_tensor: torch.Tensor, block_size: int) ->
         Number of blocks of work, given a block size.
     """
     # get the stride of the last dimension
-    stride = input_tensor.stride()[-2]
+    stride = input_tensor.stride(-2)
     numel = input_tensor.numel()
     total_blocks = math.ceil(numel / stride)
-    return math.ceil(total_blocks / block_size)
+    return total_blocks
 
 
 def unravel_index(tensor: torch.Tensor, index: int) -> tuple[int, ...]:
