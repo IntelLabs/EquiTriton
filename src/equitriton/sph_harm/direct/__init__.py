17b0234fa5cccb3621b27737c2df88ed1356a72a
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-08-22 11:39:28 -0700

    feat: exposing tenth order to direct namespace

--- src/equitriton/sph_harm/direct/__init__.py
+++ src/equitriton/sph_harm/direct/__init__.py
@@ -1,4 +1,9 @@
 from equitriton.sph_harm.direct.y_2 import SecondOrderSphericalHarmonic
 from equitriton.sph_harm.direct.y_5 import FifthOrderSphericalHarmonic
+from equitriton.sph_harm.direct.y_10 import TenthOrderSphericalHarmonic
 
-__all__ = ["SecondOrderSphericalHarmonic", "FifthOrderSphericalHarmonic"]
+__all__ = [
+    "SecondOrderSphericalHarmonic",
+    "FifthOrderSphericalHarmonic",
+    "TenthOrderSphericalHarmonic",
+]
