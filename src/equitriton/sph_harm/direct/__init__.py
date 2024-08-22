ddb01380de1cb19c12cd5b2e6d4a9be41aa49390
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-08-22 11:54:13 -0700

    chore: exposing utility function to direct namespace

--- src/equitriton/sph_harm/direct/__init__.py
+++ src/equitriton/sph_harm/direct/__init__.py
@@ -1,9 +1,11 @@
 from equitriton.sph_harm.direct.y_2 import SecondOrderSphericalHarmonic
 from equitriton.sph_harm.direct.y_5 import FifthOrderSphericalHarmonic
 from equitriton.sph_harm.direct.y_10 import TenthOrderSphericalHarmonic
+from equitriton.sph_harm.direct.utils import triton_spherical_harmonic
 
 __all__ = [
     "SecondOrderSphericalHarmonic",
     "FifthOrderSphericalHarmonic",
     "TenthOrderSphericalHarmonic",
+    "triton_spherical_harmonic",
 ]
