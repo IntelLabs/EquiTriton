690cd43c87b55194646e9f67dd2bafe56dc39f90
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-09-01 21:21:46 -0700

    refactor: ninth order with col offset and output stride

--- src/equitriton/sph_harm/direct/y_9.py
+++ src/equitriton/sph_harm/direct/y_9.py
@@ -502,6 +502,8 @@ def ninth_order_fwd(
     block_size: tl.constexpr,
     coord_numel: tl.constexpr,
     output_numel: tl.constexpr,
+    col_offset: tl.constexpr,
+    output_stride: tl.constexpr,
 ):
     # these are hardcoded because they are predetermined;
     coord_stride = 3
@@ -918,9 +920,10 @@ def ninth_order_fwd(
         + CONST091 * VAR06 * VAR23
         + CONST105 * VAR08 * VAR21
     )
-    output_stride = 19  # [2l + 1]
     output_striding = tl.arange(0, block_size) * output_stride
-    output_row_offset = output_striding + (block_size * output_stride * block_id)
+    output_row_offset = (
+        output_striding + (block_size * output_stride * block_id) + col_offset
+    )
     tl.store(output_ptr + output_row_offset, Y00, mask=output_row_offset < output_numel)
     tl.store(
         output_ptr + output_row_offset + 1,
@@ -1022,6 +1025,8 @@ def ninth_order_bwd(
     block_size: tl.constexpr,
     coord_numel: tl.constexpr,
     output_numel: tl.constexpr,
+    col_offset: tl.constexpr,
+    output_stride: tl.constexpr,
 ):
     # work out the row offsets
     block_id = tl.program_id(0)
@@ -1037,9 +1042,10 @@ def ninth_order_bwd(
     z = tl.load(
         coord_ptr + coord_row_offset + 2, mask=coord_row_offset + 2 < coord_numel
     )
-    output_stride = 19  # [2l + 1]
     output_striding = tl.arange(0, block_size) * output_stride
-    output_row_offset = output_striding + (block_size * output_stride * block_id)
+    output_row_offset = (
+        output_striding + (block_size * output_stride * block_id) + col_offset
+    )
     # load in gradients w.r.t. spherical harmonic projections
     g_0 = tl.load(
         sph_grad_ptr + output_row_offset, mask=output_row_offset < output_numel
