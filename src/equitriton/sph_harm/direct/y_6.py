12fd185ce26940408bbed5fae332c42d041125f0
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-09-01 21:11:30 -0700

    refactor: allowing sixth order to accept col offset and output stride

--- src/equitriton/sph_harm/direct/y_6.py
+++ src/equitriton/sph_harm/direct/y_6.py
@@ -196,6 +196,8 @@ def sixth_order_fwd(
     block_size: tl.constexpr,
     coord_numel: tl.constexpr,
     output_numel: tl.constexpr,
+    col_offset: tl.constexpr,
+    output_stride: tl.constexpr,
 ):
     # these are hardcoded because they are predetermined;
     coord_stride = 3
@@ -326,9 +328,10 @@ def sixth_order_fwd(
         + CONST043 * VAR04
         - CONST043 * VAR22
     )
-    output_stride = 13  # [2l + 1]
     output_striding = tl.arange(0, block_size) * output_stride
-    output_row_offset = output_striding + (block_size * output_stride * block_id)
+    output_row_offset = (
+        output_striding + (block_size * output_stride * block_id) + col_offset
+    )
     tl.store(output_ptr + output_row_offset, Y00, mask=output_row_offset < output_numel)
     tl.store(
         output_ptr + output_row_offset + 1,
@@ -400,6 +403,8 @@ def sixth_order_bwd(
     block_size: tl.constexpr,
     coord_numel: tl.constexpr,
     output_numel: tl.constexpr,
+    col_offset: tl.constexpr,
+    output_stride: tl.constexpr,
 ):
     # work out the row offsets
     block_id = tl.program_id(0)
@@ -415,9 +420,10 @@ def sixth_order_bwd(
     z = tl.load(
         coord_ptr + coord_row_offset + 2, mask=coord_row_offset + 2 < coord_numel
     )
-    output_stride = 13  # [2l + 1]
     output_striding = tl.arange(0, block_size) * output_stride
-    output_row_offset = output_striding + (block_size * output_stride * block_id)
+    output_row_offset = (
+        output_striding + (block_size * output_stride * block_id) + col_offset
+    )
     # load in gradients w.r.t. spherical harmonic projections
     g_0 = tl.load(
         sph_grad_ptr + output_row_offset, mask=output_row_offset < output_numel
