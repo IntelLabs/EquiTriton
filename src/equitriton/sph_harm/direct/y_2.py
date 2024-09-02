eb699b285e4b4a6c0f1712e8db259734f8ac2d7e
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-09-01 21:39:05 -0700

    fix: making second order add to gradients instead of overwrite

--- src/equitriton/sph_harm/direct/y_2.py
+++ src/equitriton/sph_harm/direct/y_2.py
@@ -210,14 +210,23 @@ def second_order_bwd(
     g_Y24 = tl.load(
         sph_grad_ptr + output_row_offset + 4, mask=output_row_offset + 4 < output_numel
     )
-    g_x = (
+    g_x = tl.load(
+        coord_grad_ptr + coord_row_offset, mask=coord_row_offset < coord_numel
+    )
+    g_y = tl.load(
+        coord_grad_ptr + coord_row_offset + 1, mask=coord_row_offset + 1 < coord_numel
+    )
+    g_z = tl.load(
+        coord_grad_ptr + coord_row_offset + 2, mask=coord_row_offset + 2 < coord_numel
+    )
+    g_x += (
         CONST_00 * g_Y20 * z
         + CONST_00 * g_Y21 * y
         - CONST_01 * g_Y22 * x
         - CONST_00 * g_Y24 * x
     )
-    g_y = CONST_00 * g_Y21 * x + CONST_02 * g_Y22 * y + CONST_00 * g_Y23 * z
-    g_z = (
+    g_y += CONST_00 * g_Y21 * x + CONST_02 * g_Y22 * y + CONST_00 * g_Y23 * z
+    g_z += (
         CONST_00 * g_Y20 * x
         - CONST_01 * g_Y22 * z
         + CONST_00 * g_Y23 * y
