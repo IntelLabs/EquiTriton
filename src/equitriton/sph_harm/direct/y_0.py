1f25519cebbb23815e96581b0107904d4b227d48
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2024-09-01 21:36:29 -0700

    refactor: correcting zeroth order bwd for consistency

--- src/equitriton/sph_harm/direct/y_0.py
+++ src/equitriton/sph_harm/direct/y_0.py
@@ -104,7 +104,9 @@ def zeroth_order_fwd(
 
 @triton.jit
 def zeroth_order_bwd(
-    output_ptr: tl.tensor,
+    coord_ptr: tl.tensor,
+    coord_grad_ptr: tl.tensor,
+    sph_grad_ptr: tl.tensor,
     block_size: tl.constexpr,
     coord_numel: tl.constexpr,
     output_numel: tl.constexpr,
